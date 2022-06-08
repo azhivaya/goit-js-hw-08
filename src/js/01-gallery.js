9a1942a62910ffa499493cd89eae2ed464ecf128
Alina Zhivaya <zhivayaav@gmail.com>
2022-06-08 22:13:33 +0300

    add libs

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,21 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItem(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<div class="gallery__item"><a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a></div>`;  
+    }).join('');
+}
+
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: '250' });
