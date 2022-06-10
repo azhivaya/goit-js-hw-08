78006be351ceccdc81f98f8d388622a8058da8a8
Alina Zhivaya <zhivayaav@gmail.com>
2022-06-10 17:32:33 +0300

    tasks resolving

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(currentSec, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+function currentSec(e) {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+};
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
+
+const LS_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+const formData = {};
+
+form.addEventListener('submit', onFormSubmit);
+email.addEventListener('input', throttle(onTextareaInput, 500));
+message.addEventListener('input', throttle(onTextareaInput, 500));
+
+if (localStorage.getItem(LS_KEY) !== null) {
+    const parsedData = JSON.parse(localStorage.getItem(LS_KEY));
+
+    email.value = parsedData.email;
+    message.value = parsedData.message;
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    const inputData = {
+        email: email.value,
+        message: message.value,
+    }
+
+    if (email.value === '' || message.value === '') {
+        return alert('fullfill all fields')
+    } else {
+    console.log(inputData);
+    localStorage.removeItem(LS_KEY);
+    e.currentTarget.reset(); 
+};  
+}
+
+function onTextareaInput(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(LS_KEY, JSON.stringify(formData));
+}
 
